e8b87b073f6406cef30e88b2f0bf3eec3d384292
Shailja Srivastava <shailjavastav89@gmail.com>
2021-05-20 09:42:23 +0530

    Warm-up

---
+++ Warm-up Challenges/RepeatedString.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    c=0
+    l=len(s)
+    for i in range(0,l):
+        if s[i]=='a':
+            c=c+1
+    c*= int(n / l)
+    for i in range(0, n % l):
+        if s[i] == 'a':
+            c+= 1
+    return c
+          
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
